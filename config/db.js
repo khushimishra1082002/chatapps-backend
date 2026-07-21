6454121c6c61d67197fa6913d6fa2dad51f2081d
khushi <khushimishra1082002@gmail.com>
2026-07-20 22:47:52 -0700

    fix sequelize dialect

--- config/db.js
+++ config/db.js
@@ -1,20 +1,47 @@
+// require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+
+// console.log("process.env.DB_NAME", process.env.DB_NAME);
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   process.env.CLOUDINARY_CLOUD_NAME,
+//   process.env.CLOUDINARY_API_KEY,
+//   process.env.CLOUDINARY_API_SECRET,
+//   {
+//     host: process.env.DB_HOST || "localhost",
+//     dialect: "mysql",
+//     logging: false,
+//   },
+// );
+
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Database connected successfully");
+//   } catch (err) {
+//     console.error("Unable to connect database", err);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = { sequelize, connectDB };
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-console.log("process.env.DB_NAME", process.env.DB_NAME);
+console.log("DB:", process.env.DB_NAME);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.CLOUDINARY_CLOUD_NAME,
-  process.env.CLOUDINARY_API_KEY,
-  process.env.CLOUDINARY_API_SECRET,
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql",
     logging: false,
-  },
+  }
 );
 
 const connectDB = async () => {
@@ -22,9 +49,12 @@ const connectDB = async () => {
     await sequelize.authenticate();
     console.log("Database connected successfully");
   } catch (err) {
-    console.error("Unable to connect database", err);
-    process.exit(1);
+    console.error("Database connection failed:", err);
+    throw err;
   }
 };
 
-module.exports = { sequelize, connectDB };
+module.exports = {
+  sequelize,
+  connectDB,
+};
