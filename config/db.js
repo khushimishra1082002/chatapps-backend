8c4a3060f83bad0f3c06bae33f3e9e638f310f15
khushi <khushimishra1082002@gmail.com>
2026-07-22 03:50:35 -0700

    hange in dbs

--- config/db.js
+++ config/db.js
@@ -60,6 +60,39 @@
 //   connectDB,
 // };
 
+// require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+// const mysql2 = require("mysql2");
+
+// console.log("DB:", process.env.DB_NAME);
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     dialectModule: mysql2,
+//     logging: false,
+//   }
+// );
+
+// const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Database connected successfully");
+//   } catch (err) {
+//     console.error("Database connection failed:", err);
+//     throw err;
+//   }
+// };
+
+// module.exports = {
+//   sequelize,
+//   connectDB,
+// };
+
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const mysql2 = require("mysql2");
@@ -72,11 +105,22 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectModule: mysql2,
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },},
   }
 );
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PORT);
+
 
 const connectDB = async () => {
   try {
